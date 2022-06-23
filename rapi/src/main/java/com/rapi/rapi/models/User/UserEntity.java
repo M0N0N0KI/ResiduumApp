f2d138c19e5a63209ad89c5adfb70040fee9e08c
MONONOKI <kevin.cv.br@gmail.com>
2022-06-22 21:04:31 -0400

    adding the contact's and address foreign key

--- rapi/src/main/java/com/rapi/rapi/models/User/UserEntity.java
+++ rapi/src/main/java/com/rapi/rapi/models/User/UserEntity.java
@@ -7,7 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.rapi.rapi.models.Address.AddressEntity;
+import com.rapi.rapi.models.Contact.ContactEntity;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,22 +21,34 @@ import lombok.Setter;
 @Setter
 
 @Entity
-@Table(name = "user")
+@Table(name = "userr")
 public class UserEntity implements Serializable{
 
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
+    @Column(name = "userr_id")
     private long id;
 
-    @Column(name = "user_name")
+    @Column(name = "userr_name")
     private String name;
 
-    @Column(name = "user_identifier")
+    @Column(name = "userr_identifier")
     private String identifier;
 
-    @Column(name = "user_status")
+    @Column(name = "userr_password",columnDefinition = "TEXT")
+    private String password;
+
+    @Column(name = "userr_status")
     private String status;
+
+    @OneToOne
+    @JoinColumn(name = "userr_contact")
+    private ContactEntity contact;
+
+    @OneToOne
+    @JoinColumn(name = "userr_address")
+    private AddressEntity address;
+
 }
