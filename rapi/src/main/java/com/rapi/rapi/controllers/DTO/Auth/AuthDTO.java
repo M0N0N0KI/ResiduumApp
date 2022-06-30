81212d93be6af6600ca27be36ab511e4ca67322e
M0N0N0KI <kevin.cv.br@gmail.com>
2022-06-30 10:55:43 -0400

    Auth DTO creation

---
+++ rapi/src/main/java/com/rapi/rapi/controllers/DTO/Auth/AuthDTO.java
@@ -0,0 +1,13 @@
+package com.rapi.rapi.controllers.DTO.Auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class AuthDTO {
+    
+    private String identifier;
+    private String password;
+}
