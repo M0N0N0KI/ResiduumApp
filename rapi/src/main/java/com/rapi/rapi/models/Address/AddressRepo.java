ea948bd8e84ef7bcd92f303046a66a29048e4771
M0N0N0KI <kevin.cv.br@gmail.com>
2022-07-15 11:01:31 -0400

    correção

--- rapi/src/main/java/com/rapi/rapi/models/Address/AddressRepo.java
+++ rapi/src/main/java/com/rapi/rapi/models/Address/AddressRepo.java
@@ -1,15 +1,10 @@
 package com.rapi.rapi.models.Address;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepo extends JpaRepository<AddressEntity, Long>{
-    
-    @Query("select A from AddressEntity A where A.hnumber = ?1 and A.postalcode = ?2 and A.latitude = ?3 and A.longitude = ?4")
-    public Optional<AddressEntity> verseExiste(String hnumber, String postalCode, String latitude, String longitude);
 
 }
